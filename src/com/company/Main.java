11347e275be854655968fe6b7ea8da5713967307
barsha <barsha.sangraula.2022@students.lhschools.org>
2020-02-28 11:52:51 -0500

    intellij commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is");
+        System.out.println("Barsha Sangraula");
+
+        System.out.println("I am 16 years old");
+        System.out.println("I am in 10Th grade" );
+        System.out.println("I like to play sports" );
+        System.out.println("I also like to go shopping ");
+        System.out.println("Last summer I went to different states");
+        System.out.println("I met my boyfriend ");
+        System.out.println("I spent good times with my family ");
+    }
+}
